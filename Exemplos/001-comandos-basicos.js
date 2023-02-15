7150439993d2a12eb7af4a44300fb1d785340d09
Jessicapapidim <124743212+Jessicapapidim@users.noreply.github.com>
2023-02-14 21:20:22 -0300

    Aula variáveis

---
+++ Exemplos/001-comandos-basicos.js
@@ -0,0 +1,39 @@
+//comentário de uma linha só
+console.log('Hello world');
+
+/* comentário
+de mais de um alinha
+*/
+
+/* pesquisar a diferença entre as instruções
+var, let e const na linguagem java-script
+*/
+
+let nome= "Jessica";
+let idade= 31;
+let altura= 1.57;
+let trabalha = false;
+let corfavorita= "Rosa";
+let cores_favoritas= ["rosa","roxo","amarelo","laranja"]
+let endereco= {
+    "logradouro" : "Rua",
+    "nome-log" : "Brasil",
+    "n" : "105-A",
+    "bairro" : "centro"
+};
+
+let covid;
+let dengue = null;
+
+const exfunc = () => {};
+
+console.log("variavel nome=" + typeof (nome));
+console.log("variavel idade=" + typeof (idade));
+console.log("variavel altura=" + typeof (altura));
+console.log("variavel trabalha=" + typeof (trabalha));
+console.log("variavel corfavorita=" + typeof (corfavorita));
+console.log("variavel cores_favoritas=" + typeof (cores_favoritas));
+console.log("variavel endereco=" + typeof (endereco));
+console.log("variavel covid=" + typeof (covid));
+console.log("variavel dengue=" + typeof (dengue));
+console.log("variavel exfunc=" + typeof (exfunc));      
