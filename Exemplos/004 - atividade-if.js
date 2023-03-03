be1af2b11a3be5107e19c394c6d4b4d2ebbd977c
Jessicapapidim <124743212+Jessicapapidim@users.noreply.github.com>
2023-03-03 19:23:54 -0300

    Conclusão atividade

--- Exemplos/004 - atividade-if.js
+++ Exemplos/004 - atividade-if.js
@@ -1,11 +1,22 @@
 let nota = require('prompt-sync')();
 
 let vDigitado;
-vDigitado = nota('Digite a 1º nota: ')
+vDigitado = nota('Digite a 1º nota de 0 a 10: ')
 let n1 = parseInt(vDigitado);
-vDigitado = nota('Digite o 2º nota: ')
+vDigitado = nota('Digite o 2º nota de 0 a 10: ')
 let n2 = parseInt(vDigitado);
 
-media = (n1 + n2)/2
+let media = (n1 + n2)/2
+
+if (media < 5) {
+    console.log ("I");
+} else if (media >= 5 && media < 6.5) {
+    console.log ("R")
+} else if (media >= 6.5 && media < 8.5){
+    console.log ("B");
+} else {
+    console.log ("MB")
+}
+
 
 
